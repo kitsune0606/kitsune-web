e18dfe20d28538dcba24f3619521bc9eb7c8a800
PEN <uemasaki20011222@gmail.com>
2025-11-28 20:11:43 +0900

    JS修正

--- kitsune-parts-js/header.js
+++
@@ -1,5 +0,0 @@
-
-        fetch("./kitsune-parts/header.html")
-         .then(res=>res.text())//HTML を取り込む
-         .then(html=>document.getElementBuId("site-header").insertAdjacentHTML("beforeend",html))
-         //header.html の中身を <header id="site-header"> に追加する
